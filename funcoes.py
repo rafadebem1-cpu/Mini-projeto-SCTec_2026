64c2b1a0199c13a7bc52add0ad0c06d614ee38e8
rafadebem1-cpu <rafadebem1@gmail.com>
2026-06-07 21:33:53 -0300

    Projeto RF01 a RF14 concluído

---
+++ funcoes.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import datetime
+import random
+from datetime import datetime, timedelta
+
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import re
+
+def gerar_dataset_vendas():
+    ...
+
+def inspecionar_dados(df):
+    ...
+
+def limpar_dados(df):
+    ...
+
+def transformar_dados(df):
+    ...
+
+def calcular_metricas_agregadas(df):
+    ...
+
+def segmentar_clientes(df):
+    ...
+
+def calcular_estatisticas_numpy(df):
+    ...
+
+def executar_analise_poo(df):
+    ...
+
+def gerar_graficos(df):
+    ...
+
+def exportar_arquivos(df):
+    ...
+def main():
+    ...
